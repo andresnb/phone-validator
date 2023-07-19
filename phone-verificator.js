ad26907b3e036f938af1b50c04add7d97f27ca4b
Andres Naula <andresnaula@gmail.com>
2023-07-19 18:43:09 -0500

    add: validate phone numebr funcition simple validation

--- phone-verificator.js
+++ phone-verificator.js
@@ -7,5 +7,23 @@ function restrictInput(event) {
   }
 }
 
+function validatePhoneNumber() {
+  const phoneNumber = document.getElementById('phoneNumber').value;
+
+  const isValidPhoneNumber = /^\+?\d/.test(phoneNumber) 
+
+  if (isValidPhoneNumber) {
+      alert('Phone number is valid!');
+  } else {
+      alert('Invalid phone number! Please enter a valid phone number starting with "+" and containing at least one digit.');
+  }
+}
+
 const phoneNumber = document.getElementById('phoneNumber');
+const form = document.getElementById('form');
+
 phoneNumber.addEventListener('keypress', restrictInput);
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    validatePhoneNumber();
+});
